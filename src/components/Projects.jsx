b0b8f16a21c66ded302e9e2ce3b90f38967222d0
Edson Tovar <102639181+EdsonTovaR@users.noreply.github.com>
2025-11-29 23:33:53 -0600

    Actualizar Projects.jsx

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -26,7 +26,7 @@ const Projects = () => {
     },
     {
       title: "Nutrición Gym",
-      category: "Aplicación Web",
+      category: "Aplicacion Web",
       description: "Sistema web integral para gestión de gimnasio, incluyendo control de inventarios, registro de miembros, seguimiento de ventas y control de gastos.",
       tech: ["Flask", "Python", "SQLite", "Bootstrap"],
       color: "from-orange-400 to-red-500",
